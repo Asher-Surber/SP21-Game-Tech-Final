82762e31b6cd338e389e6fd76189d9c11072a590
CalDough <77810418+CalDough@users.noreply.github.com>
2021-04-24 13:27:11 -0400

    Work in Progress

--- Project/Global.gd
+++ Project/Global.gd
@@ -1,5 +1,5 @@
 extends Node
-
+var mouse = null
 
 func _unhandled_input(event):
 	if event.is_action_pressed("quit"):
@@ -7,4 +7,11 @@ func _unhandled_input(event):
 	#if event.is_action_pressed("pause menu"):
 		#get_tree().pause()
 		#get_tree().change_scene("res://Menus/Pause Menu.tscn")
-	
+	if mouse == null:
+		if event.is_action_pressed("mouse"):
+			mouse = Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		
+	if mouse != null:
+		if event.is_action_pressed("mouse"):
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			mouse = null
