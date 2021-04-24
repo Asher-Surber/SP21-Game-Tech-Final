0abcfc314d619431a88a74a893e210a8fd46b0c4
CalDough <77810418+CalDough@users.noreply.github.com>
2021-04-24 14:35:27 -0400

    Mouse Function

--- Project/Global.gd
+++ Project/Global.gd
@@ -1,5 +1,5 @@
 extends Node
-var mouse = null
+var mouseset = true
 
 func _unhandled_input(event):
 	if event.is_action_pressed("quit"):
@@ -7,11 +7,10 @@ func _unhandled_input(event):
 	#if event.is_action_pressed("pause menu"):
 		#get_tree().pause()
 		#get_tree().change_scene("res://Menus/Pause Menu.tscn")
-	if mouse == null:
-		if event.is_action_pressed("mouse"):
-			mouse = Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-		
-	if mouse != null:
-		if event.is_action_pressed("mouse"):
+	if event.is_action_pressed("mouse"):
+		if mouseset == false:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+			mouseset = true
+		else:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-			mouse = null
+			mouseset = false
