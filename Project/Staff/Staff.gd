d7550ffb320db44a0292d40c425f23a889dcaf25
Asher Surber <ashersurber@gmail.com>
2021-04-24 17:48:39 -0400

    commit

--- Project/Staff/Staff.gd
+++ Project/Staff/Staff.gd
@@ -8,6 +8,7 @@ extends Spatial
 	
 onready var SmallBlast = load("res://Staff/SmallBlast.tscn")
 onready var container = get_node("/root/Level1/BlastContainer")
+onready var Staff = load("res://Staff/Staff.tscn")
 
 export(int) var crystal_count
 export(int) var mana = 100
@@ -22,9 +23,12 @@ export(int) var mana = 100
 
 
 func smallBlast():
-	if Input.is_action_just_pressed("fire"):
-		if mana >= 5:
-			var small_Blast = container.instance(SmallBlast)
-			mana -= 5
-		else:
-			return "But nothing happened!"	#need to create popup message for empty mana
+	if Input.is_action_pressed("fire"):
+#		if mana >= 5:
+		var small_Blast = SmallBlast.instance()
+		container.add_child(small_Blast)
+		small_Blast.position = Staff.position
+		small_Blast.velocity = Vector3(2,2,2)
+#			mana -= 5
+#		else:
+#			return "But nothing happened!"	#need to create popup message for empty mana
--- Project/Staff/smallBlast.gd
+++ Project/Staff/smallBlast.gd
@@ -1,10 +1,10 @@
 extends Area
 
-var velocity = Vector3(0,0,0)
-var position = get_viewport().get_mouse_position()
+#var velocity = Vector3(0,0,0)
+#var position = get_viewport().get_mouse_position()
 
-func _physics_process(_delta):
-	velocity += 5
+#func _physics_process(_delta):
+#	velocity += 5
 
 
 func _on_SmallBlast_body_entered(body):
